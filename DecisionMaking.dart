14e5695589a09ff7ac8188703407b6523609ad9d
Your Name <you@example.com>
2021-07-20 20:42:50 +0600

    if,else..if,if..else in Dart

---
+++ DecisionMaking.dart
@@ -0,0 +1,22 @@
+void main(){
+
+
+   var age =29;
+
+
+    if(age>18){
+
+         print("Adult");
+    }
+    else if (age<10){
+
+        print("Child");
+
+    }
+    else{
+
+        print("Invalid");
+
+
+    }
+}
